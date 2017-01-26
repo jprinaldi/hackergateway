01b3da01827da1d6107d7955cc1626c967f7d703
Juan Pablo Rinaldi <juampi.rinaldi@gmail.com>
2017-01-26 09:34:43 -0300

    Add seed data to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+u = User.new
+u.email = 'guest@example.com'
+u.password = 'aaaaaa'
+u.confirm
+u.save
+
+crypto = Category.create(name: 'Cryptography', icon: 'lock')
+stegano = Category.create(name: 'Steganography', icon: 'picture-o')
+
+Challenge.create(name: 'Test', body: '<strong>hello, world</strong>', answer: 'testing', category: crypto)
+Challenge.create(name: 'Test 2', body: '<em>goodbye, world</em>', answer: 'testing2', category: stegano)
