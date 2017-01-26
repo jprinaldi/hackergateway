b194a533f5a67dd061cb7c42beded5dffeb254b6
Juan Pablo Rinaldi <juampi.rinaldi@gmail.com>
2017-01-25 22:23:51 -0300

    Add answer field to Challenges

--- db/migrate/20170126004654_create_challenges.rb
+++ db/migrate/20170126004654_create_challenges.rb
@@ -3,6 +3,7 @@ class CreateChallenges < ActiveRecord::Migration[5.0]
     create_table :challenges do |t|
       t.string :name
       t.text :body
+      t.string :answer
       t.references :category, foreign_key: true
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20170126004654) do
   create_table "challenges", force: :cascade do |t|
     t.string   "name"
     t.text     "body"
+    t.string   "answer"
     t.integer  "category_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
@@ -51,8 +52,10 @@ ActiveRecord::Schema.define(version: 20170126004654) do
     t.datetime "locked_at"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true, using: :btree
   end
 
   add_foreign_key "challenges", "categories"
