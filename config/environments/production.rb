69945068ddbe0383e973fa7b359259f33685d608
Juan Pablo Rinaldi <juampi.rinaldi@gmail.com>
2017-02-02 02:06:58 -0300

    Add default host for mailers

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,8 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "HackerGateway_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: 'hackergateway.com' }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
@@ -64,7 +66,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     user_name: ENV["SENDGRID_USERNAME"],
     password: ENV["SENDGRID_API_KEY"],
-    domain: 'heroku.com',
+    domain: 'hackergateway.com',
     address: 'smtp.sendgrid.net',
     port: 587,
     authentication: :plain,
