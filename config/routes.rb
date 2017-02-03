331670f1d34e2a4ac329c6f80c2229a62d746e92
Juan Pablo Rinaldi <juampi.rinaldi@gmail.com>
2017-02-03 11:26:33 -0300

    Add About section

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   get 'policies/privacy'
   get 'policies/terms'
   get 'faq', to: 'faq#index'
+  get 'about', to: 'about#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
