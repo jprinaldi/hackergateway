871c45e14712ff7faf59591b8d100b3198de37e1
Juan Pablo Rinaldi <juampi.rinaldi@gmail.com>
2017-02-02 22:06:10 -0300

    Add privacy policy and terms of service

---
+++ app/controllers/policies_controller.rb
@@ -0,0 +1,10 @@
+class PoliciesController < ApplicationController
+  def index
+  end
+
+  def privacy
+  end
+
+  def terms
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,9 @@ Rails.application.routes.draw do
   resources :solutions, only: :index
   resources :categories
   root to: 'home#index'
+  get 'policies', to: 'policies#index'
+  get 'policies/privacy'
+  get 'policies/terms'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
