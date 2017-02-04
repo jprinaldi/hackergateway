209e4a5139267cf6acef6bc80b5d85617ee0653c
Juan Pablo Rinaldi <juampi.rinaldi@gmail.com>
2017-02-04 12:56:31 -0300

    Improve Home section

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
-    @newest_challenges = Challenge.last(5)
-    @newest_users = User.last(5)
+    @newest_challenges = Challenge.last(5).reverse
+    @newest_solutions = Solution.includes(:user, :challenge).last(5).reverse
+    @newest_users = User.last(5).reverse
   end
 end
