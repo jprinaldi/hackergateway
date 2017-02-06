3fbbff31f11f9f35fc035927cb988f95d9e03359
Juan Pablo Rinaldi <juampi.rinaldi@gmail.com>
2017-02-06 18:20:33 -0300

    Reorder User attributes

--- db/migrate/20170125211545_devise_create_users.rb
+++ db/migrate/20170125211545_devise_create_users.rb
@@ -5,6 +5,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
+      # Custom attributes
+      t.string :username
+      t.string :country_code
+      t.integer :solutions_count, default: 0
+      t.string :slug
+
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
@@ -29,11 +35,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
       t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
       t.string   :unlock_token # Only if unlock strategy is :email or :both
       t.datetime :locked_at
-
-      t.string :username
-      t.string :country_code
-      t.integer :solutions_count, default: 0
-      t.string :slug
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -73,6 +73,10 @@ ActiveRecord::Schema.define(version: 20170126181922) do
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
+    t.string   "username"
+    t.string   "country_code"
+    t.integer  "solutions_count",        default: 0
+    t.string   "slug"
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -88,10 +92,6 @@ ActiveRecord::Schema.define(version: 20170126181922) do
     t.integer  "failed_attempts",        default: 0,  null: false
     t.string   "unlock_token"
     t.datetime "locked_at"
-    t.string   "username"
-    t.string   "country_code"
-    t.integer  "solutions_count",        default: 0
-    t.string   "slug"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
