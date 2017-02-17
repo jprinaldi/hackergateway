cd51678360a9da087321356949aed1b6ad5c3eea
Juan Pablo Rinaldi <juampi.rinaldi@gmail.com>
2017-02-17 16:00:37 -0300

    Improve ApiKey model implementation

--- app/models/api_key.rb
+++ app/models/api_key.rb
@@ -1,13 +1,4 @@
 # This model represents API keys consisting of access tokens
 class ApiKey < ApplicationRecord
-  before_create :generate_token
-
-  private
-
-  def generate_token
-    loop do
-      self.token = SecureRandom.hex
-      break unless self.class.exists?(token: token)
-    end
-  end
+  has_secure_token
 end
