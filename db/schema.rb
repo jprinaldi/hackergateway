3b80db934ba9545196d5557a5287b32a7f9788b5
Juan Pablo Rinaldi <juampi.rinaldi@gmail.com>
2017-05-02 17:00:11 -0300

    Update bundle

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,6 @@ ActiveRecord::Schema.define(version: 20170303150222) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "pg_stat_statements"
 
   create_table "api_keys", force: :cascade do |t|
     t.string   "token"
