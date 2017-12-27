97a87176eab693854c1179fa4fe6aca05c2da9b3
Juan Pablo Rinaldi <juampi.rinaldi@gmail.com>
2017-12-27 12:23:34 -0300

    Remove default order for challenge solutions

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -5,7 +5,7 @@ class Challenge < ApplicationRecord # :nodoc:
   friendly_id :name, use: :slugged
 
   belongs_to :category, counter_cache: true
-  has_many :solutions, -> { order(created_at: :desc) }, dependent: :destroy
+  has_many :solutions, dependent: :destroy
   has_many :solvers, through: :solutions, source: :user, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
