5c67000490668da9d729913ebb9515ae7ca3d8f7
Juan Pablo Rinaldi <juampi.rinaldi@gmail.com>
2018-01-09 20:55:54 -0300

    Replace jquery_ujs with rails-ujs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require rails-ujs
 //= require jquery3
-//= require jquery_ujs
 //= require bootstrap/dist/js/bootstrap.bundle
 //= require timeago/jquery.timeago
 //= require @fortawesome/fontawesome
