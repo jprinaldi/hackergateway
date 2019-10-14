7a8b340b4b3331e8ce76bb72bb15306c4a3d9cba
Juan Pablo Rinaldi <juampi.rinaldi@gmail.com>
2019-10-14 09:19:07 -0300

    Remove unnecessary middleware plug

--- config/application.rb
+++ config/application.rb
@@ -13,9 +13,6 @@ module HackerGateway
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    # Use Rack::Attack for blocking & throttling abusive requests
-    config.middleware.use Rack::Attack
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
