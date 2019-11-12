cfbeab0828c632a9c7490cc7aca2b0005df1d5a2
Juan Pablo Rinaldi <juampi.rinaldi@gmail.com>
2019-11-12 18:29:13 -0300

    Bump sprockets from 3.7.2 to 4.0.0

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,5 @@
 //= link_tree ../images
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
+//= link_directory ../images/favicon .json
+//= link_directory ../images/favicon .xml
