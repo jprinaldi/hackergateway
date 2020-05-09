31a08f99ec5a01a6f7da518529c1b8f7104c526e
Juan Pablo Rinaldi <juampi.rinaldi@gmail.com>
2020-05-09 16:28:51 -0300

    Add SameSite cookie configuration

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,4 +2,4 @@
 
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: "_HackerGateway_session"
+Rails.application.config.session_store :cookie_store, key: "_HackerGateway_session", same_site: :strict
