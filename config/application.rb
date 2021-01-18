f3c372f91f2d8f33694b07452d50a4ee12d7fbdc
Juan Pablo Rinaldi <juampi.rinaldi@gmail.com>
2021-01-18 11:35:40 -0300

    Run rails app:update

--- config/application.rb
+++ config/application.rb
@@ -5,16 +5,20 @@ require_relative "boot"
 require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development or :production.
+# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module HackerGateway
   class Application < Rails::Application # :nodoc:
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 6.1
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
