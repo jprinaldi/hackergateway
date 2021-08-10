3c768482601e2ef4e878bbb379ec95aa0dc9c93e
Juan Pablo Rinaldi <jprinaldi@protonmail.com>
2021-08-10 10:48:08 -0300

    Remove non-inclusive terminology

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -9,8 +9,8 @@ class Rack::Attack # rubocop:disable Style/ClassAndModuleChildren
   # If you don't want to use Rails.cache (Rack::Attack's default), then
   # configure it here.
   #
-  # Note: The store is only used for throttling (not blacklisting and
-  # whitelisting). It must implement .increment and .write like
+  # Note: The store is only used for throttling (not allowlisting and
+  # denylisting). It must implement .increment and .write like
   # ActiveSupport::Cache::Store
 
   # Rack::Attack.cache.store = ActiveSupport::Cache::MemoryStore.new
