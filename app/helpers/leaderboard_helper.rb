2af9a705cee7a16981e452887d268b5eb97c9e92
Juan Pablo Rinaldi <jprinaldi@protonmail.com>
2021-09-14 08:47:22 -0300

    Fix RuboCop issue

--- app/helpers/leaderboard_helper.rb
+++ app/helpers/leaderboard_helper.rb
@@ -2,6 +2,6 @@
 
 module LeaderboardHelper # :nodoc:
   def leaderboard_rank(index, page, per)
-    (index + 1) + ((page || 1).to_i - 1) * per.to_i
+    (index + 1) + (((page || 1).to_i - 1) * per.to_i)
   end
 end
