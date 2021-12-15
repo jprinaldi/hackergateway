33f28e4d25b3b7ad46389e65cdc157e0f9283dc7
Juan Pablo Rinaldi <jprinaldi@protonmail.com>
2021-12-14 17:20:21 -0800

    Update db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -22,11 +22,11 @@ ActiveRecord::Schema.define(version: 2021_01_18_142138) do
     t.bigint "resource_id"
     t.string "author_type"
     t.bigint "author_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author"
     t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
-    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
+    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource"
   end
 
   create_table "active_storage_attachments", force: :cascade do |t|
