dc040b032ff04fba434b0e5ce9f719276b1731e8
Juan Pablo Rinaldi <jprinaldi@protonmail.com>
2021-12-18 00:07:58 -0800

    Add Forest Admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  mount ForestLiana::Engine => "/forest"
   devise_for :users, controllers: { registrations: :registrations }
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
