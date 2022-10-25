620bb30971532deb83fcf57efba90f12177310cb
Juan Pablo Rinaldi <jprinaldi@protonmail.com>
2022-10-24 21:57:16 -0300

    Remove unuseful routing specs

--- spec/routing/faqs_routing_spec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-RSpec.describe FaqsController, type: :routing do
-  describe "routing" do
-    it "routes to #index" do
-      expect(get: "/faqs").to route_to("faqs#index")
-    end
-  end
-end
--- spec/routing/solutions_routing_spec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-RSpec.describe SolutionsController, type: :routing do
-  describe "routing" do
-    it "routes to #index" do
-      expect(get: "/solutions").to route_to("solutions#index")
-    end
-  end
-end
