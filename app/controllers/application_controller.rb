43893548d66185c2820b254666062c9bbec57261
Juan Pablo Rinaldi <jprinaldi@protonmail.com>
2022-11-09 05:41:54 -0300

    Improve error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  rescue_from ActiveRecord::RecordNotFound, with: :handle_record_not_found
+
+  def handle_record_not_found
+    render "errors/not_found", status: :not_found
+  end
+
   protected
 
   def configure_permitted_parameters
